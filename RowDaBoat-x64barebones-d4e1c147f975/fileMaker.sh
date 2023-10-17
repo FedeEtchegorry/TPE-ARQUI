1f41c36013c3db0969546bd8924197db7d75a75f
FedeEtchegorry <115056584+FedeEtchegorry@users.noreply.github.com>
2023-10-17 17:57:16 -0300

    Revert "Create fileMaker.sh"
    This reverts commit 5a34915f89cc8e46f1a2742382c7db2b9901a55e.

--- RowDaBoat-x64barebones-d4e1c147f975/fileMaker.sh
+++
@@ -1,6 +0,0 @@
-docker start CONTAINER 
-docker exec -it CONTAINER make clean -C/root/Toolchain 
-docker exec -it CONTAINER make clean -C/root/ 
-docker exec -it CONTAINER make -C/root/Toolchain 
-docker exec -it CONTAINER make -C/root/ 
-docker stop CONTAINER
