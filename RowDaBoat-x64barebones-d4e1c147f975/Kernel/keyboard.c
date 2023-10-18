d652b483d877f9f848d27816856f4cd52a046379
MaxiChiate <chiatellinomaximo@gmail.com>
2023-10-18 18:04:01 -0300

    .

--- RowDaBoat-x64barebones-d4e1c147f975/Kernel/keyboard.c
+++ RowDaBoat-x64barebones-d4e1c147f975/Kernel/keyboard.c
@@ -1,7 +1,7 @@
 #include <keyboard.h>
 
 
-static kbd_US [128] =   {
+static unsigned char kbd_US [128] =   {
             0,  '|', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '\?', '!', '\b',   
         '\t', /* <-- Tab */
         'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '*', '+', '\n',     
