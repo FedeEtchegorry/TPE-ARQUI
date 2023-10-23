5e2a9e391a7399103d3a93618090327f800c2aec
FedeEtchegorry <115056584+FedeEtchegorry@users.noreply.github.com>
2023-10-23 15:52:00 -0300

    colors

--- RowDaBoat-x64barebones-d4e1c147f975/Kernel/include/colours.h
+++ RowDaBoat-x64barebones-d4e1c147f975/Kernel/include/colours.h
@@ -4,20 +4,11 @@
     #define collapseFB(fore, back) ( ((back) << 4) | (fore) )
 
     #define BLACK           0x00
-    #define BLUE            0x01
-    #define GREEN           0x02
-    #define CYAN            0x03
-    #define RED             0x04
-    #define MAGENTA         0x05
-    #define BROWN           0x06
-    #define LIGHT_GRAY      0x07
-    #define DARK_GRAY       0x08
-    #define LIGHT_BLUE      0x09
-    #define LIGHT_GREEN     0x0A
-    #define LIGHT_CYAN      0x0B
-    #define LIGHT_RED       0x0C
-    #define LIGHT_MAGENTA   0x0D
-    #define YELLOW          0x0E
-    #define WHITE           0x0F
+    #define BLUE            0x0004FF
+    #define GREEN           0x04FF00
+    #define RED             0xFF0C00
+    #define MAGENTA         0xFF00FF
+    #define YELLOW          0xFFF300
+    #define WHITE           0xFFFFFF
 
 #endif
