52f9e6fbe2f4eaaf3b9ceb16d9abdcb08d6941ba
MaxiChiate <chiatellinomaximo@gmail.com>
2023-10-31 14:45:12 -0300

    snake tester (recommended)

--- RowDaBoat-x64barebones-d4e1c147f975/Userland/SampleCodeModule/sampleCodeModule.c
+++ RowDaBoat-x64barebones-d4e1c147f975/Userland/SampleCodeModule/sampleCodeModule.c
@@ -1,21 +1,34 @@
 #include <userlib.h>
 #include <shell.h>
+#include <snake.h>
 
 int main() {
 
 
-	initShell();
+// 	initShell();
 
-	char buffer[254] = {'\0'};
+// 	char buffer[254] = {'\0'};
 
-	do	{
-	read(buffer);
-    time();
-    print(buffer);
-}	while(!strEquals(buffer, "exit"));
+// 	do	{
+// 	read(buffer);
+//     time();
+//     print(buffer);
+// }	while(!strEquals(buffer, "exit"));
 
 
-	exit();
+// 	exit();
+
+	tSnake mySnake;
+	spawnSnake(mySnake);
+	printSnakeInfo(mySnake);	
+
+	changeSnakeDirection(mySnake, UP);
+	print("Change dir to UP"); putnEnters(2);
+	printSnakeInfo(mySnake);
+
+	print("Move");putnEnters(2);
+	moveSnake(mySnake);
+	printSnakeInfo(mySnake);
 
 	return 0;
 }
