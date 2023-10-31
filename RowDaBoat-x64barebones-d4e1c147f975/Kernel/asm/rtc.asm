23ef58a94d3082e44627de2e8d03f2514739824d
FedeEtchegorry <115056584+FedeEtchegorry@users.noreply.github.com>
2023-10-31 03:53:23 -0300

    .

--- RowDaBoat-x64barebones-d4e1c147f975/Kernel/asm/rtc.asm
+++ RowDaBoat-x64barebones-d4e1c147f975/Kernel/asm/rtc.asm
@@ -14,43 +14,6 @@ timeParam:                  ;;en rax se tendria que poner el parámetro del tiem
     xor rax, rax
     in al, 71h
 
-
-
     mov rsp, rbp
     pop rbp
-    ret
-
-getSeconds:
-    mov al, 0h
-    out 70h, al
-	xor rax, rax
-	in al, 71h
-    ret
-
-getMinutes:
-    mov al, 2h
-    out 70h, al
-    in al, 71h
-    ret
-
-getHours:
-    mov al, 4h
-    out 70h, al
-    in al, 71h
-    ret
-
-getDay:
-    mov al, 7h
-    out 70h, al
-    in al, 71h
-    ret
-getMonth:
-    mov al, 8h
-    out 70h, al
-    in al, 71h
-    ret
-getYear:
-    mov al, 9h
-    out 70h, al
-    in al, 71h
     ret
